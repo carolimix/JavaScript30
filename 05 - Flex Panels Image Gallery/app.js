94e6583a9dedd50241b5c8b639686ed18b2c58cf
Carolina <carolina.acuna90@gmail.com>
2024-01-09 19:18:47 +0100

    flex y eventListenners

---
+++ 05 - Flex Panels Image Gallery/app.js
@@ -0,0 +1,18 @@
+const panels = document.querySelectorAll(".panel");
+
+function toggleOpen() {
+  console.log("toggleOpen");
+  this.classList.toggle("open");
+}
+
+function toggleActive(e) {
+  console.log("toggleActive", e.propertyName);
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
+
+panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+panels.forEach((panel) =>
+  panel.addEventListener("transitionend", toggleActive)
+);
